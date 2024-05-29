7ec2d89a6c3b85591f85c1489f835c8c894e7e33
Перевязко Алина <seongdiffr@mail.ru>
2024-05-29 15:11:03 +0700

    Количество дней с даты рождения

---
+++ lab8/get-days-count.js
@@ -0,0 +1,14 @@
+function showDaysCount() {
+    let today = new Date();
+    let inputDate = document.querySelector('input[type=date]');
+    let birthday = new Date(inputDate.value);
+    let daysCount = (today - birthday)/1000/60/60/24;
+    daysCount = Math.floor(daysCount);
+    document.getElementById('count-days').textContent = 'Количество дней с даты рождения: ' + daysCount;
+}
+
+function clearInput() {
+    let inputDate = document.querySelector('input[type=date]');
+    inputDate.value = '';
+    document.getElementById('count-days').textContent = '';
+}
